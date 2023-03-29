57da9b8ae177a20ce04e73cda17e98eb4667e4e8
Roman <romaleks360@gmail.com>
2023-03-29 20:15:57 +0300

    add FPS counter

---
+++ Runtime/Components/FPSCounter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Ostium11
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        void OnGUI()
+        {
+            int fps = (int)(1f / Time.deltaTime);
+            GUI.skin.label.fontSize = 48;
+            GUI.Label(new Rect(50, 50, 500, 100), fps.ToString());
+        }
+    }
+}
