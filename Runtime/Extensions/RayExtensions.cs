8f57d3e9f21075dde9e6e92fb6ea309eb50ca063
Roman <romaleks360@gmail.com>
2023-04-10 14:18:10 +0300

    add RayExtensions (closest point between two rays)

---
+++ Runtime/Extensions/RayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Ostium11.Extensions
+{
+    public static class RayExtensions
+    {
+        // https://palitri.com/vault/stuff/maths/Rays%20closest%20point.pdf - first formula
+        /// <summary>
+        /// Calculates the closest point between two rays.
+        /// </summary>
+        /// <returns> Distance along the first ray </returns>
+        public static float GetClosestDistanceTo(this Ray from, Ray to)
+        {
+            var a = from.direction;
+            var b = to.direction;
+            var c = to.origin - from.origin;
+            return (-Dot(a, b) * Dot(b, c) + Dot(a, c) * Dot(b, b)) / (Dot(a, a) * Dot(b, b) - Dot(a, b) * Dot(a, b));
+
+            static float Dot(Vector3 a, Vector3 b) => Vector3.Dot(a, b);
+        }
+    }
+}
