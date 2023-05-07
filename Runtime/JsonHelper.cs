e9633111e4accbc6bb49044c213a7a186a0ef68d
Roman <romaleks360@gmail.com>
2023-05-07 22:47:17 +0300

    add json helper

---
+++ Runtime/JsonHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ostium11
+{
+    public static class JsonHelper
+    {
+        [Serializable] class JsonArray<T> { public T[] arr; }
+
+        public static T[] ArrayFromJson<T>(string json) => JsonUtility.FromJson<JsonArray<T>>(string.Concat("{\"arr\":", json, "}")).arr;
+
+        public static string ArrayToJson<T>(T[] arr) => JsonUtility.ToJson(new JsonArray<T>() { arr = arr })[7..^1];
+    }
+}
