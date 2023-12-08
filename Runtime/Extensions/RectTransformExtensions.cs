93a825683709e9decb29c4d2c1962ac9763ac9da
Roman <romaleks360@gmail.com>
2023-12-08 22:53:53 +0300

    add RectTransform.GetWorldRect

--- Runtime/Extensions/RectTransformExtensions.cs
+++ Runtime/Extensions/RectTransformExtensions.cs
@@ -4,6 +4,8 @@ namespace Ostium11.Extensions
 {
     public static class RectTransformExtensions
     {
+        static readonly Vector3[] _corners = new Vector3[4];
+
         /// <summary>
         /// Set pivot without changing the position of the element
         /// </summary>
@@ -23,6 +25,12 @@ namespace Ostium11.Extensions
             rectTransform.anchorMin = anchorPoint;
             rectTransform.anchorMax = anchorPoint;
             rectTransform.localPosition = pos;
+        }
+
+        public static Rect GetWorldRect(this RectTransform rectTransform)
+        {
+            rectTransform.GetWorldCorners(_corners);
+            return new Rect(_corners[0], _corners[2] - _corners[0]);
         }
     }
 }
