0cfb929ea54ac0b969082ede612ea091669ba14e
Roman <romaleks360@gmail.com>
2024-04-06 19:32:57 +0300

    add Comparison enum and Compare method

---
+++ Runtime/Comparison.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Ostium11
+{
+    [Flags]
+    public enum Comparison
+    {
+        Less = 1,
+        Equal = 2,
+        Greater = 4,
+        LessOrEqual = Less | Equal,
+        NotEqual = Less | Greater,
+        GreaterOrEqual = Greater | Equal,
+    }
+
+    public static class ComparisonExtensions
+    {
+        public static bool Compare(this Comparison comparison, float val1, float val2) =>
+            ((int)comparison & (1 << (Math.Sign(val1.CompareTo(val2)) + 1))) != 0;
+
+        public static bool Compare(this Comparison comparison, int val1, int val2) =>
+            ((int)comparison & (1 << (Math.Sign(val1.CompareTo(val2)) + 1))) != 0;
+    }
+}
