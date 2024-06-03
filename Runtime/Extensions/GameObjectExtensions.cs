94e60c3cccf9587d9a16915ebf7cfc1dfb12d257
Roman <romaleks360@gmail.com>
2024-06-03 22:58:47 +0300

    fix GetOrAddComponent return value

--- Runtime/Extensions/GameObjectExtensions.cs
+++ Runtime/Extensions/GameObjectExtensions.cs
@@ -4,7 +4,7 @@ namespace Ostium11.Extensions
 {
     public static class GameObjectExtensions
     {
-        public static Component GetOrAddComponent<T>(this GameObject go) where T : Component
+        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
         {
             if (!go.TryGetComponent(out T component))
                 component = go.AddComponent<T>();
