b60432bb4068376402230c15b1750bfcb9f19955
Roman <romaleks360@gmail.com>
2024-06-06 21:35:29 +0300

    add TransformExtensions

---
+++ Runtime/Extensions/TransformExtensions.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Ostium11.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void SetX(this Transform transform, float x) => transform.position = transform.position.SetX(x);
+
+        public static void SetY(this Transform transform, float y) => transform.position = transform.position.SetY(y);
+
+        public static void SetZ(this Transform transform, float z) => transform.position = transform.position.SetZ(z);
+
+        public static Transform GetLastChild(this Transform transform) => transform.GetChild(transform.childCount - 1);
+    }
+}
