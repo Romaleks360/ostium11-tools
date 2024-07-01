0e56cd9464c80445187d5fcd6f8ab8e119360d67
maksim-belousov <belousov1999@gmail.com>
2024-07-01 18:57:53 +0300

    Added comparison sign extension

--- Runtime/Comparison.cs
+++ Runtime/Comparison.cs
@@ -20,5 +20,16 @@ namespace Ostium11
 
         public static bool Compare(this Comparison comparison, int val1, int val2) =>
             ((int)comparison & (1 << (Math.Sign(val1.CompareTo(val2)) + 1))) != 0;
+        
+        public static string Sign(this Comparison comparison) => comparison switch
+        {
+            Comparison.Less => "<",
+            Comparison.Equal => "=",
+            Comparison.Greater => ">",
+            Comparison.LessOrEqual => "<=",
+            Comparison.NotEqual => "!=",
+            Comparison.GreaterOrEqual => ">=",
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
 }
