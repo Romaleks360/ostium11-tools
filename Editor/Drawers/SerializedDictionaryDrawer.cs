830243da8b30718f9641fb77a1b38c3ebc4dd7d3
Roman <romaleks360@gmail.com>
2024-07-14 15:51:24 +0300

    revert to newer reorderable list drawer from max

--- Editor/Drawers/SerializedDictionaryDrawer.cs
+++ Editor/Drawers/SerializedDictionaryDrawer.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
@@ -7,29 +8,28 @@ namespace Ostium11.Editors
     [CustomPropertyDrawer(typeof(SerializedDictionary<,>))]
     public class SerializedDictionaryDrawer : PropertyDrawer
     {
+        const float verticalPadding = 5f;
+        const float horizontalSpacing = 3f;
         const float foldoutHeaderHeight = 20f;
         const float arraySizeWidth = 48f;
 
-        bool _initialized;
-        ReorderableList _list;
+        readonly Dictionary<string, ReorderableList> _reorderableLists = new();
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            if (!_initialized)
-            {
-                _initialized = true;
-                Init(property);
-            }
+            Init(property);
 
             float height = base.GetPropertyHeight(property, label);
             if (property.isExpanded)
-                height += _list.GetHeight();
+                height += _reorderableLists[property.propertyPath].GetHeight();
             return height;
         }
 
         public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
         {
-            var foldoutRect = new Rect(position) { height = foldoutHeaderHeight };
+            Init(prop);
+
+            var foldoutRect = EditorGUI.IndentedRect(new Rect(position) { height = foldoutHeaderHeight });
             prop.isExpanded = EditorGUI.BeginFoldoutHeaderGroup(foldoutRect, prop.isExpanded, prop.displayName);
             EditorGUI.EndFoldoutHeaderGroup();
 
@@ -47,154 +47,152 @@ namespace Ostium11.Editors
 
             position.y += foldoutHeaderHeight;
             if (prop.isExpanded)
-                _list.DoList(position);
+                _reorderableLists[prop.propertyPath].DoList(position);
         }
 
         void Init(SerializedProperty prop)
         {
-            var keysType = fieldInfo.FieldType.GetGenericArguments()[0];
+            if (_reorderableLists.ContainsKey(prop.propertyPath))
+                return;
+
+            var unconstructedType = typeof(SerializedDictionary<,>);
+            System.Type keysType = null;
+
+            Queue<System.Type> queueOfTypes = new Queue<System.Type>();
+            foreach (var t in fieldInfo.FieldType.GetGenericArguments())
+                queueOfTypes.Enqueue(t);
+
+            while (queueOfTypes.Count > 0)
+            {
+                keysType = queueOfTypes.Dequeue();
+
+                if (keysType.IsGenericType)
+                {
+                    if (unconstructedType == keysType.GetGenericTypeDefinition())
+                    {
+                        keysType = keysType.GetGenericArguments()[0];
+                        break;
+                    }
+                    else
+                        foreach (var t in keysType.GetGenericArguments())
+                            queueOfTypes.Enqueue(t);
+                }
+                else
+                    break;
+            }
+
             bool isEnum = keysType.IsEnum;
 
-            if (isEnum)
-                InitEnumKeys(prop, keysType);
-
-            _list = new ReorderableList(prop.serializedObject, prop.FindPropertyRelative("_pairs"),
+            ReorderableList reorderableList = null;
+            reorderableList = new ReorderableList(prop.serializedObject, prop.FindPropertyRelative("_pairs"),
                 draggable: !isEnum,
                 displayHeader: true,
                 displayAddButton: !isEnum,
                 displayRemoveButton: !isEnum)
             {
-                drawHeaderCallback = DrawHeader,
-                drawElementCallback = DrawElement,
+                drawElementCallback = OnDrawElement,
                 drawElementBackgroundCallback = DrawElementBackground,
-                elementHeightCallback = GetElementHeight,
+                elementHeightCallback = OnElementHeight,
+                drawHeaderCallback = OnDrawHeader
             };
-        }
+            _reorderableLists.Add(prop.propertyPath, reorderableList);
 
-        void InitEnumKeys(SerializedProperty prop, System.Type keysType)
-        {
-            var pairs = prop.FindPropertyRelative("_pairs");
-            var enumValues = keysType.GetEnumValues();
-
-            if (pairs.arraySize == enumValues.Length)
-                return;
-
-            pairs.ClearArray();
-            foreach (int val in enumValues)
+            void OnDrawHeader(Rect rect)
             {
-                pairs.InsertArrayElementAtIndex(pairs.arraySize);
-                var key = pairs.GetArrayElementAtIndex(pairs.arraySize - 1).FindPropertyRelative("key");
-                key.enumValueIndex = val;
-            }
-            pairs.serializedObject.ApplyModifiedPropertiesWithoutUndo();
-        }
-
-        void DrawHeader(Rect rect)
-        {
-            var keysRect = new Rect(rect)
-            {
-                x = rect.x + 15,
-                width = rect.width * .25f
-            };
-
-            var valuesRect = new Rect(keysRect)
-            {
-                x = keysRect.x + keysRect.width + 20
-            };
-
-            EditorGUI.LabelField(keysRect, new GUIContent("Keys"));
-            EditorGUI.LabelField(valuesRect, new GUIContent("Values"));
-        }
-
-        float GetElementHeight(int index)
-        {
-            if (_list.count == 0)
-                return 0;
-
-            var pair = _list.serializedProperty.GetArrayElementAtIndex(index);
-            var key = pair.FindPropertyRelative("key");
-            var value = pair.FindPropertyRelative("value");
-
-            return Mathf.Max(EditorGUI.GetPropertyHeight(key), EditorGUI.GetPropertyHeight(value));
-        }
-
-        void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
-        {
-            if (_list.count == 0)
-                return;
-
-            var pair = _list.serializedProperty.GetArrayElementAtIndex(index);
-            var key = pair.FindPropertyRelative("key");
-            var value = pair.FindPropertyRelative("value");
-
-            if (key == null || value == null)
-            {
-                Debug.LogError("Error displaying dictionary data. Make sure you are using a [Serializable] data type!");
-                return;
-            }
-
-            if (!_list.draggable)
-                rect.xMin += 15f;
-
-            var keyRect = new Rect()
-            {
-                x = rect.x,
-                y = rect.y + .5f,
-                width = rect.width * .2f,
-                height = EditorGUI.GetPropertyHeight(key)
-            };
-
-            var arrowRect = new Rect(keyRect)
-            {
-                x = keyRect.xMax,
-                width = 35,
-            };
-
-            var valueRect = new Rect()
-            {
-                x = arrowRect.xMax,
-                y = arrowRect.y,
-                width = rect.width - keyRect.width - arrowRect.width,
-                height = EditorGUI.GetPropertyHeight(value)
-            };
-
-            if (value.hasVisibleChildren)
-                valueRect.xMin += 15f;
-
-            if (key.propertyType == SerializedPropertyType.Enum)
-                EditorGUI.LabelField(keyRect, key.enumDisplayNames[key.enumValueIndex]);
-            else
-                EditorGUI.PropertyField(keyRect, key, GUIContent.none, true);
-
-            EditorGUI.LabelField(arrowRect, new GUIContent("⟹"), new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter });
-
-            EditorGUIUtility.labelWidth /= 2f;
-            EditorGUI.PropertyField(valueRect, value, value.hasVisibleChildren ? new GUIContent("Value") : GUIContent.none, true);
-            EditorGUIUtility.labelWidth = 0f;
-        }
-
-        void DrawElementBackground(Rect rect, int index, bool isActive, bool isFocused)
-        {
-            if (Event.current.type != EventType.Repaint || index == -1)
-                return;
-
-            var pairs = _list.serializedProperty;
-            var key = pairs.GetArrayElementAtIndex(index).FindPropertyRelative("key");
-
-            for (int i = 0; i < index; i++)
-            {
-                var otherKey = pairs.GetArrayElementAtIndex(i).FindPropertyRelative("key");
-                if (SerializedProperty.DataEquals(key, otherKey))
+                var keysRect = new Rect(rect)
                 {
-                    // same key, draw red background
-                    EditorGUI.DrawRect(rect, isFocused ? Color.HSVToRGB(0, 1, 1) : Color.HSVToRGB(0, 1, 0.7f));
-                    return;
-                }
+                    x = rect.x + 15,
+                    width = rect.width * .25f
+                };
+
+                var valuesRect = new Rect(keysRect)
+                {
+                    x = keysRect.x + keysRect.width + 20
+                };
+
+                EditorGUI.LabelField(keysRect, new GUIContent("Keys"));
+                EditorGUI.LabelField(valuesRect, new GUIContent("Values"));
             }
 
-            // draw usual background
-            GUIStyle s = "RL Element";
-            s.Draw(rect, isHover: false, isActive, isActive, isFocused);
+            float OnElementHeight(int index)
+            {
+                if (reorderableList.count == 0)
+                    return 0;
+
+                var pair = reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+                SerializedProperty key = pair.FindPropertyRelative(nameof(key));
+                SerializedProperty value = pair.FindPropertyRelative(nameof(value));
+
+                return Mathf.Max(EditorGUI.GetPropertyHeight(key), EditorGUI.GetPropertyHeight(value)) + EditorGUIUtility.standardVerticalSpacing * 2;
+            }
+
+            void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused)
+            {
+                if (reorderableList.count == 0)
+                    return;
+
+                var pair = reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+                SerializedProperty key = pair.FindPropertyRelative(nameof(key));
+                SerializedProperty value = pair.FindPropertyRelative(nameof(value));
+
+                var keyRect = new Rect()
+                {
+                    y = rect.y + verticalPadding * .5f,
+                    x = rect.x,
+                    width = rect.width * .25f,
+                    height = EditorGUI.GetPropertyHeight(key)
+                };
+
+                var arrowRect = new Rect(keyRect)
+                {
+                    y = keyRect.y - verticalPadding * .25f,
+                    x = rect.x + horizontalSpacing + keyRect.width,
+                    width = 35,
+                };
+
+                var valueRect = new Rect(keyRect)
+                {
+                    x = arrowRect.x + arrowRect.width,
+                    width = rect.width - keyRect.width - arrowRect.width,
+                    height = EditorGUI.GetPropertyHeight(value)
+                };
+
+                if (key.propertyType == SerializedPropertyType.Enum)
+                    EditorGUI.LabelField(keyRect, key.enumDisplayNames[key.enumValueIndex]);
+                else
+                    EditorGUI.PropertyField(keyRect, key, GUIContent.none, true);
+
+                EditorGUI.LabelField(arrowRect, new GUIContent("⟹"));
+
+                EditorGUIUtility.labelWidth /= 2;
+                var name = value.FindPropertyRelative("name")?.stringValue;
+                EditorGUI.PropertyField(valueRect, value, name == null ? GUIContent.none : new GUIContent(name), true);
+                EditorGUIUtility.labelWidth = 0;
+            }
+
+            void DrawElementBackground(Rect rect, int index, bool isActive, bool isFocused)
+            {
+                if (Event.current.type != EventType.Repaint || index == -1)
+                    return;
+
+                var pairs = reorderableList.serializedProperty;
+                var key = pairs.GetArrayElementAtIndex(index).FindPropertyRelative("key");
+
+                for (int i = 0; i < index; i++)
+                {
+                    var otherKey = pairs.GetArrayElementAtIndex(i).FindPropertyRelative("key");
+                    if (SerializedProperty.DataEquals(key, otherKey))
+                    {
+                        // same key, draw red background
+                        EditorGUI.DrawRect(rect, isFocused ? Color.HSVToRGB(0, 1, 1) : Color.HSVToRGB(0, 1, 0.7f));
+                        return;
+                    }
+                }
+
+                // draw usual background
+                GUIStyle s = "RL Element";
+                s.Draw(rect, isHover: false, isActive, isActive, isFocused);
+            }
         }
     }
 }
