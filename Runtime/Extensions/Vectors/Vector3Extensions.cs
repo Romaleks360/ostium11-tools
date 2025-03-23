ef227ae90ad5b1ab323ae00ab278d27463e2e30f
Roman <romaleks360@gmail.com>
2025-03-23 21:33:05 +0300

    add Vector3 Add... extensions

--- Runtime/Extensions/Vectors/Vector3Extensions.cs
+++ Runtime/Extensions/Vectors/Vector3Extensions.cs
@@ -40,6 +40,24 @@ namespace Ostium11.Extensions
             return vector;
         }
 
+        public static Vector3 AddX(this Vector3 vector, float x)
+        {
+            vector.x += x;
+            return vector;
+        }
+
+        public static Vector3 AddY(this Vector3 vector, float y)
+        {
+            vector.y += y;
+            return vector;
+        }
+
+        public static Vector3 AddZ(this Vector3 vector, float z)
+        {
+            vector.z += z;
+            return vector;
+        }
+
         public static Vector3 Set(this Vector3 vector, int axis, float value)
         {
             vector[axis] = value;
