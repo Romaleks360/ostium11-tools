b3b7c5620166138a04ab82d5b3e250296cefe79c
stalengd <p.zamorskiy@yandex.ru>
2025-05-09 16:23:02 +0300

    Newtonsoft.Json for SerializedDictionary

--- Runtime/Collections/SerializedDictionary.cs
+++ Runtime/Collections/SerializedDictionary.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace Ostium11
 {
     [System.Serializable]
+    [JsonObject]
     public class SerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
     {
         [SerializeField] List<Pair> _pairs = new();
@@ -31,53 +34,13 @@ namespace Ostium11
                 _pairs.Add(new Pair(k, v));
         }
 
-        [System.Serializable]
-        struct Pair
-        {
-            public TKey key;
-            public TValue value;
-
-            public Pair(TKey key, TValue value)
-            {
-                this.key = key;
-                this.value = value;
-            }
-        }
-    }
-
-    [System.Serializable]
-    public class ReferenceSerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver where TValue : class
-    {
-        [SerializeField] List<Pair> _pairs = new();
-
-        public void OnAfterDeserialize()
-        {
-            Clear();
-            foreach (var pair in _pairs)
-                if (!ContainsKey(pair.key))
-                    Add(pair.key, pair.value);
-        }
-
-        public void OnBeforeSerialize()
-        {
-            if (!Application.isPlaying)
-                return;
-
-            ForceUpdateSerializedData();
-        }
-
-        public void ForceUpdateSerializedData()
-        {
-            _pairs.Clear();
-            foreach (var (k, v) in this)
-                _pairs.Add(new Pair(k, v));
-        }
+        [OnDeserialized] public void OnDeserialized(StreamingContext context) => OnAfterDeserialize();
+        [OnSerializing] public void OnSerializing(StreamingContext context) => OnBeforeSerialize();
 
         [System.Serializable]
         struct Pair
         {
             public TKey key;
-            [SerializeReference]
             public TValue value;
 
             public Pair(TKey key, TValue value)
