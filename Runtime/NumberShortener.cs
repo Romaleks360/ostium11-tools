b881a8dc3e8ff179f2fa81b3467ef43de84af628
stalengd <p.zamorskiy@yandex.ru>
2025-07-14 16:44:25 +0300

    NumberShortener

---
+++ Runtime/NumberShortener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Ostium11
+{
+    public static class NumberShortener
+    {
+        public static string ToShortString(float value, string format = "0.#")
+        {
+            var (order, postfix) = Mathf.Abs(value) switch
+            {
+                >= 1_000_000_000 => (1_000_000_000, "B"),
+                >= 1_000_000 => (1_000_000, "M"),
+                >= 1_000 => (1_000, "K"),
+                _ => (1, ""),
+            };
+            return (value / order).ToString(format) + postfix;
+        }
+    }
+}
